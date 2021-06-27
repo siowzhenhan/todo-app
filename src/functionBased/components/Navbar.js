be84d7ce415730646a7a30e9f3f9f8a7f3dc4f71
Zhen Han Siow <zhen.han@omnimatics.io>
2021-06-27 11:23:53 +0800

    Adds navbar with avatar and logout button

--- src/functionBased/components/Navbar.js
+++ src/functionBased/components/Navbar.js
@@ -1,34 +1,36 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import Avatar from '@material-ui/core/Avatar'
+import { useHistory } from 'react-router-dom'
 
 const Navbar = () => {
-  // all links
-  const links = [
-    {
-      id: 1,
-      path: '/',
-      text: 'Home'
-    },
-    {
-      id: 2,
-      path: '/about',
-      text: 'About'
-    }
-  ]
+  const user = sessionStorage.getItem('user')
+  const history = useHistory()
 
   return (
     <nav className='navBar'>
-      <ul>
-        {links.map(link => {
-          return (
-            <li key={link.id}>
-              <NavLink to={link.path} activeClassName='active-link' exact>
-                {link.text}
-              </NavLink>
-            </li>
-          )
-        })}
-      </ul>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <Avatar atl={user} style={{ margin: 10 }} />
+        <span style={{ color: '#6D8187' }}>{user}</span>
+      </div>
+
+      <button
+        style={{
+          lineHeight: '1.5rem',
+          width: '5rem',
+          margin: 10,
+          borderRadius: 10,
+          border: 'none',
+          background: 'transparent',
+          color: '#6D8187',
+          cursor: 'pointer'
+        }}
+        onClick={() => {
+          sessionStorage.clear()
+          history.push('/login')
+        }}
+      >
+        Logout
+      </button>
     </nav>
   )
 }
